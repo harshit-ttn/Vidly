edc36c4590f95cbee7b2d0be24042610e93636d9
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-28 18:46:23 +0530

    Encapsulating Logic in Mongoose Models
    Information Expert Principle

--- models/user.js
+++ models/user.js
@@ -1,31 +1,38 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
 
-const User = mongoose.model('User', new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
+  email:{
+      type:String,
+      require: true,
       minlength: 5,
-      maxlength: 50
-    },
+      maxlength: 255,
+      unique: true
+  },
 
-    email:{
-        type:String,
-        require: true,
-        minlength: 5,
-        maxlength: 255,
-        unique: true
-    },
+  password:{
+      type:String,
+      require: true,
+      minlength: 5,
+      maxlength: 1024,
+  }
+});
 
-    password:{
-        type:String,
-        require: true,
-        minlength: 5,
-        maxlength: 1024,
-    }
-  })
-  );
+userSchema.methods.generateAuthToken = function(){
+  const token = jwt.sign({_id:this._id}, config.get('jwtPrivateKey'));
+  return token;
+}
+
+const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
   const schema = {
