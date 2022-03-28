9b57edbf6534a1b15a0f1c08f69ae4b8577a8899
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-28 19:16:02 +0530

    Authorization Middleware
    Here we implement authorization feature using middleware function

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,8 @@ const config = require('config');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+
+    
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
     
