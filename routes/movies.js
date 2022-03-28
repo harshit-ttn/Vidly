030dd63d34476ed265e3448854fa091114c3be16
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-28 11:50:59 +0530

    Validating Object ID's

--- routes/movies.js
+++ routes/movies.js
@@ -30,6 +30,7 @@ router.post('/', async (req, res) => {
   res.send(movie);
 });
 
+
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
